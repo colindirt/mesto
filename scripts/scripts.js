ea70e667cf4e484d5b22d6304f5179943794cbf3
Denis Sapozhnikov <colindir.t@yandex.ru>
2023-01-22 02:27:30 +0800

    Исправлены замечания ревьюера

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,8 +2,8 @@ const popup = document.querySelector ('.popup');
 const editButton = document.querySelector ('.profile__edit-button');
 const closeButton = document.querySelector ('.popup__close');
 let formElement = document.querySelector ('.popup__form');
-let nameInput = document.querySelector('.popup__input_name');
-let jobInput = document.querySelector('.popup__input_job');
+let nameInput = document.querySelector('.popup__input-name');
+let jobInput = document.querySelector('.popup__input-job');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
 
